9494fd69b8ac5f72fbefb7808facf3522b1f5360
Draquo <draquo.drass@gmail.com>
2022-12-20 09:41:34 +0100

    Party are now finishable

--- func.py
+++ func.py
@@ -2,16 +2,26 @@ import random
 
 def generatecoord():
     SHIP_COORD = {}
+    list_y = []
     x = random.randint(1, 5)
     y = random.randint(1, 10)
     SHIP_COORD["5x1"] = []
     for i in range(5):
         SHIP_COORD["5x1"].append((x, y))
         x = x + 1
+    list_y.append(y)
     x = random.randint(1, 6)
-    y = random.randint(1, 10)
+    while y in list_y:
+        y = random.randint(1, 10)
     SHIP_COORD["4x1"] = []
     for i in range(4):
         SHIP_COORD["4x1"].append((x, y))
         x = x + 1
     return SHIP_COORD
+
+def aishoot(shotsmade):
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+    while (x, y) in shotsmade:
+        x, y = aishoot(shotsmade)
+    return (x, y)
--- main.py
+++ main.py
@@ -50,12 +50,16 @@ SHIP_FOUR.multiplesize(1.5, X)
 SHIP_FOUR.set_defaultpos(pixelinhd(334.7, X), pixelinhd(607, X))
 
 EMPTY_CASE = []
-COORD_EMPTY_CASE_P2 = []
+# By player 2
+COORD_EMPTY_CASE_P1 = []
+COORD_TOUCHED_CASE_P1 = []
 TOUCHED_CASE = []
+COORD_EMPTY_CASE_P2 = []
 COORD_TOUCHED_CASE_P2 = []
 
 LIST_SHIP = [SHIP_FIVE, SHIP_FOUR]
 SHIP_COORD_P1 = {}
+SHIP_COORD_P2 = {}
 """
 0: No shot made
 1: Shot here but no ship
@@ -85,7 +89,9 @@ GRID_LIST_P2 = [
 [0,0,0,0,0,0,0,0,0,0]]
 
 RUNNING = True
+pass_stats = False
 stats = 0
+txt = ""
 """
 Diffrents stats:
 MENU: 0
@@ -95,9 +101,12 @@ PLAYER_1_RESULT: 3
 PLAYER_2_PLAY: 4
 PLAYER_2_RESULT: 5
 END: 6
+CLOSING GAME: 7
 """
 
 while RUNNING:
+
+    #print(f"Stats: {stats}")
 
     SCREEN.blit(FOND, (0, 0))
 
@@ -127,6 +136,57 @@ while RUNNING:
             SCREEN.blit(i.image, i.position)
         for i in TOUCHED_CASE:
             SCREEN.blit(i.image, i.position)
+    elif stats == 3:
+        showtext(SCREEN, txt, FONT, X//2, Y//2, (0, 0, 0))
+    elif stats == 4:
+        SCREEN.blit(GRID_P1.image, GRID_P1.position)
+        SCREEN.blit(GRID_P2.image, GRID_P2.position)
+        SCREEN.blit(PLAYER1_LOGO.image, PLAYER1_LOGO.position)
+        SCREEN.blit(PLAYER2_LOGO.image, PLAYER2_LOGO.position)
+        showtext(SCREEN, "Au tour de l'ia de jouer.", FONT, X//2, pixelinhd(170, X), (0, 0, 0))
+        for i in EMPTY_CASE:
+            SCREEN.blit(i.image, i.position)
+        for i in TOUCHED_CASE:
+            SCREEN.blit(i.image, i.position)
+        response = aishoot(COORD_TOUCHED_CASE_P1+COORD_EMPTY_CASE_P1)
+        nb_x = response[0]
+        nb_y = response[1]
+
+        x1 = GRID_P1.position[0]+(nb_x-1)*multiplypixelinhd(1.5, 50, X)
+        x = GRID_P1.position[0]+(nb_x-1)*multiplypixelinhd(1.5, 50, X)
+        y1 = GRID_P1.position[1]+(nb_y-1)*multiplypixelinhd(1.5, 50, X)
+        y = GRID_P1.position[1]+(nb_y-1)*multiplypixelinhd(1.5, 50, X)
+
+        case_exist = False
+        if nb_x > 0 and nb_y > 0:
+            for i in SHIP_COORD_P1.values():
+                for ii in i:
+                    if ii == (nb_x, nb_y):
+                        case_exist = True
+            if ((nb_x, nb_y) in COORD_TOUCHED_CASE_P1) == False and ((nb_x, nb_y) in COORD_EMPTY_CASE_P1) == False and case_exist:
+                txt = "touché"
+                TOUCHED_CASE.append(Pygameimage("Something case", "assets/something_touched.png"))
+                TOUCHED_CASE[-1].multiplesize(1.5, X)
+                TOUCHED_CASE[-1].set_defaultpos(x1, y1)
+                COORD_TOUCHED_CASE_P1.append((nb_x, nb_y))
+            elif ((nb_x, nb_y) in COORD_TOUCHED_CASE_P1) == False and ((nb_x, nb_y) in COORD_EMPTY_CASE_P1) == False:
+                txt = "loupé"
+                EMPTY_CASE.append(Pygameimage("Empty case", "assets/nothing_touched.png"))
+                EMPTY_CASE[-1].multiplesize(1.5, X)
+                EMPTY_CASE[-1].set_defaultpos(x1, y1)
+                COORD_EMPTY_CASE_P1.append((nb_x, nb_y))
+            length = 0
+            for i in SHIP_COORD_P1:
+                length = length + int(i[0])
+            if length == len(COORD_TOUCHED_CASE_P1):
+                winner = "l'ia"
+                stats = 6
+        stats = 5
+    elif stats == 5:
+        showtext(SCREEN, f"L'ia a {txt} son tire.", FONT, X//2, Y//2, (0, 0, 0))
+    elif stats == 6:
+        showtext(SCREEN, f"Le gagnant de match est {winner} !", FONT, X//2, Y//2, (0, 0, 0))
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -134,6 +194,20 @@ while RUNNING:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 RUNNING = False
+            elif event.key in (pygame.K_SPACE, pygame.K_RETURN):
+                if stats == 3:
+                    length = 0
+                    for i in SHIP_COORD_P2:
+                        length = length + int(i[0])
+                    if length == len(COORD_TOUCHED_CASE_P2):
+                        winner = "le joueur 1"
+                        stats = 6
+                    else:
+                        stats = 4
+                elif stats == 5:
+                    stats = 2
+                elif stats == 6:
+                    RUNNING = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == pygame.BUTTON_LEFT:
                 if colide(PLAY_BUTTON, event.pos) and stats == 0:
@@ -165,7 +239,8 @@ while RUNNING:
                         elif colide(i, event.pos):
                             i.clicked = False
                             i.set_pos(i.default_pos[0], i.default_pos[1])
-                            del SHIP_COORD_P1[i.name]
+                            if i.name in SHIP_COORD_P1.keys():
+                                del SHIP_COORD_P1[i.name]
                     if colide(CLEAR_BUTTON, event.pos):
                         for i in LIST_SHIP:
                             i.clicked = False
@@ -195,16 +270,23 @@ while RUNNING:
                                 for ii in i:
                                     if ii == (nb_x, nb_y):
                                         case_exist = True
-                            if ((nb_x, nb_y) in COORD_TOUCHED_CASE_P2) == False and ((nb_x, nb_y) in COORD_EMPTY_CASE_P2) == False and case_exist:
+                            if not((nb_x, nb_y) in COORD_EMPTY_CASE_P2+COORD_TOUCHED_CASE_P2) and case_exist:
                                 TOUCHED_CASE.append(Pygameimage("Something case", "assets/something_touched.png"))
                                 TOUCHED_CASE[-1].multiplesize(1.5, X)
                                 TOUCHED_CASE[-1].set_defaultpos(x1, y1)
                                 COORD_TOUCHED_CASE_P2.append((nb_x, nb_y))
-                            elif ((nb_x, nb_y) in COORD_TOUCHED_CASE_P2) == False and ((nb_x, nb_y) in COORD_EMPTY_CASE_P2) == False:
+                                txt = "Touché !"
+                                pass_stats = True
+                            elif not((nb_x, nb_y) in COORD_EMPTY_CASE_P2+COORD_TOUCHED_CASE_P2):
                                 EMPTY_CASE.append(Pygameimage("Empty case", "assets/nothing_touched.png"))
                                 EMPTY_CASE[-1].multiplesize(1.5, X)
                                 EMPTY_CASE[-1].set_defaultpos(x1, y1)
                                 COORD_EMPTY_CASE_P2.append((nb_x, nb_y))
+                                txt = "Loupé !"
+                                pass_stats = True
+                    if pass_stats:
+                        pass_stats = False
+                        stats = 3
 
     pygame.display.flip()
 
